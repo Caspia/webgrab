2f16396bb2dda5c0410df24f318ecbf28613c6eb
Kent James <kent@caspia.com>
2018-08-30 14:47:25 -0700

    Add forwarding commands to localroute.sh

--- localroute.sh
+++ localroute.sh
@@ -5,18 +5,34 @@
 #
 # Usage: sudo ./localroute.sh [remove]
 #
-iptables -t nat -D OUTPUT -p tcp ! -s 172.20.0.100 --dport 80 -j DNAT --to-destination 172.20.0.100:3129 2>/dev/null
-iptables -t nat -D OUTPUT -p tcp ! -s 172.20.0.100 --dport 443 -j DNAT --to-destination 172.20.0.100:3130 2>/dev/null
+
+# The address of the offlineweb server
+OFFLINEWEB_IP=172.20.0.100
+# The IP address of the dnsmasq server
+DNS_IP=172.20.0.101
+
+iptables -t nat -D OUTPUT -p tcp ! -s $OFFLINEWEB_IP --dport 80 -j DNAT --to-destination $OFFLINEWEB_IP:3129 2>/dev/null
+iptables -t nat -D OUTPUT -p tcp ! -s $OFFLINEWEB_IP --dport 443 -j DNAT --to-destination $OFFLINEWEB_IP:3130 2>/dev/null
+iptables -t nat -D PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 80 -j DNAT --to-destination $OFFLINEWEB_IP:3129 2>/dev/null
+iptables -t nat -D PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 443 -j DNAT --to-destination $OFFLINEWEB_IP:3130 2>/dev/null
+iptables -t nat -D PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 53 -j DNAT --to-destination $DNS_IP:53 2>/dev/null
+iptables -t nat -D PREROUTING -p udp ! -s $OFFLINEWEB_IP --dport 53 -j DNAT --to-destination $DNS_IP:53] 2>/dev/null
+iptables -P FORWARD DROP
 if [ "OPT $1" != "OPT remove" ]
 then
   echo "Routing web requests to local beluga/offlineweb instance"
-  iptables -t nat -A OUTPUT -p tcp ! -s 172.20.0.100 --dport 80 -j DNAT --to-destination 172.20.0.100:3129
-  iptables -t nat -A OUTPUT -p tcp ! -s 172.20.0.100 --dport 443 -j DNAT --to-destination 172.20.0.100:3130
+  iptables -t nat -A OUTPUT -p tcp ! -s $OFFLINEWEB_IP --dport 80 -j DNAT --to-destination $OFFLINEWEB_IP:3129
+  iptables -t nat -A OUTPUT -p tcp ! -s $OFFLINEWEB_IP --dport 443 -j DNAT --to-destination $OFFLINEWEB_IP:3130
+  iptables -t nat -A PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 80 -j DNAT --to-destination $OFFLINEWEB_IP:3129
+  iptables -t nat -A PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 443 -j DNAT --to-destination $OFFLINEWEB_IP:3130
+  iptables -t nat -A PREROUTING -p tcp ! -s $OFFLINEWEB_IP --dport 53 -j DNAT --to-destination $DNS_IP:53
+  iptables -t nat -A PREROUTING -p udp ! -s $OFFLINEWEB_IP --dport 53 -j DNAT --to-destination $DNS_IP:53]
+  iptables -P FORWARD ACCEPT
   if [ ! -f ".resolv.conf.old" ]
   then
     cp /etc/resolv.conf .resolv.conf.old
   fi
-  echo "nameserver 172.20.0.101" > /etc/resolv.conf
+  echo "nameserver $DNS_IP" > /etc/resolv.conf
 else
   echo "Removing routing of web requests to local beluga/offlineweb instance"
   cp .resolv.conf.old /etc/resolv.conf
